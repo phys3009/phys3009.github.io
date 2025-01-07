85657aa349e8c3507646c6deb88e07c99ad719b7
mikesmithlab <mikesmithlab@gmail.com>
2025-01-07 14:49:27 +0000

    generalised liks

--- content/constants.js
+++ content/constants.js
@@ -1,27 +1,28 @@
 const replacements = {
-  OFFICEHOUR: "Thursday from 10-11am",
+  OFFICEHOUR: "Thursday from 9-10am",
   MYWEBPAGE: "https://www.nottingham.ac.uk/~ppzmis",
   MYOFFICE: "Room C126, main physics building",
-  MYEMAIL: "mailto:mike.i.smith@nottingham.ac.uk",
+  MYEMAIL:
+    "mailto:mike.i.smith@nottingham.ac.uk?subject=Force%20and%20function%20query",
   SESSION1: "Wednesday 9-10am in Chemistry X2",
   SESSION2: "Friday 12-1pm in Chemistry X2",
   LEARNINGDIFFERENCE: "https://xerte.nottingham.ac.uk/play_23883#page1",
   EXAMKIT:
     "https://www.nottingham.ac.uk/currentstudents/documents/healthyu/2021-documents/web-640597-scom-a5-20pp-examtoolkit-update-njt-feb22.pdf",
   YELLOWBOOK:
-    "https://www.rethinkyourmind.co.uk/The-UoN-Physics-and-Astronomy-Yellow-Book-Online/?page=1"
+    "https://www.rethinkyourmind.co.uk/The-UoN-Physics-and-Astronomy-Yellow-Book-Online/?page=1",
 };
 
 function replaceValues() {
-  const replace = document.querySelectorAll('.replace'); // Select all elements with the content class
-  replace.forEach(element => {
+  const replace = document.querySelectorAll(".replace"); // Select all elements with the content class
+  replace.forEach((element) => {
     let html = element.innerHTML;
     for (const [placeholder, replacement] of Object.entries(replacements)) {
-      html = html.replace(new RegExp(placeholder, 'g'), replacement);
+      html = html.replace(new RegExp(placeholder, "g"), replacement);
     }
     element.innerHTML = html;
   });
 }
 
 // Run the replaceValues function after the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', replaceValues);
+document.addEventListener("DOMContentLoaded", replaceValues);
